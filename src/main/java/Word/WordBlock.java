f792a99ce9791b8ff78b7bd2a981ec6dff55ef68
hoangtongvu <hoangtongvu@gmail.com>
2023-10-09 08:12:32 +0700

    Update test new branch

--- src/main/java/Word/WordBlock.java
+++ src/main/java/Word/WordBlock.java
@@ -52,5 +52,6 @@ public class WordBlock implements Comparable<WordBlock>
         //throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
         return this.word.compareToIgnoreCase(o.getWord());
     }
+    
 
 }
