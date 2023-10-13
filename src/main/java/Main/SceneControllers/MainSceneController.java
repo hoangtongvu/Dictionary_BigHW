6a4a972c0a2aa18da1060f44249b9a9bb4e4d9d5
Tran Thai Duong <22028061@vnu.edu.vn>
2023-10-13 19:41:42 +0700

    Refactored GUI >> - Added drawer Menu with blurring effect >> - Added color >> - More buttons...

--- src/main/java/Main/SceneControllers/MainSceneController.java
+++ src/main/java/Main/SceneControllers/MainSceneController.java
@@ -1,13 +1,18 @@
 package Main.SceneControllers;
 
 import Dictionary.DicManager;
+import javafx.animation.FadeTransition;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
+import javafx.util.Duration;
 import org.controlsfx.control.textfield.AutoCompletionBinding;
 import org.controlsfx.control.textfield.TextFields;
 
@@ -15,8 +20,7 @@ import java.net.URL;
 import java.util.*;
 
 
-public class MainSceneController implements Initializable
-{
+public class MainSceneController implements Initializable {
 
     //private Timer timer;
     //private TimerTask timerTask;
@@ -32,8 +36,7 @@ public class MainSceneController implements Initializable
     private TextField tfTitle;
 
     @FXML
-    void btnOKClicked(ActionEvent event) 
-    {
+    void btnOKClicked(ActionEvent event) {
         //Stage mainWindow = (Stage) this.ttTitle.getScene().getWindow(); 
         //String title = this.ttTitle.getText();
         //mainWindow.setTitle(title);
@@ -57,8 +60,7 @@ public class MainSceneController implements Initializable
 
 
     @FXML
-    public void LookupWord()
-    {
+    public void LookupWord() {
         //System.out.println("null");
         String lookUpRes = DicManager.getInstance().LookUpWord(this.tfTitle.getText());//Cant find cause dic load default on button click
         Text text = new Text(lookUpRes);
@@ -68,8 +70,7 @@ public class MainSceneController implements Initializable
 
 
     @FXML
-    public void OnTextChange()
-    {
+    public void OnTextChange() {
         String text = tfTitle.getText();
         if (text == "") return;
         
@@ -85,23 +86,19 @@ public class MainSceneController implements Initializable
 
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) 
-    {
-        try 
-        {
+    public void initialize(URL location, ResourceBundle resources) {
+        blurPane.setVisible(false);
+        try {
             DicManager.getInstance().getDicWordLoader().DefaultLoad();
             
-        } catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Can't load dic file");
         }
         
         AutoCompletionBinding auto = TextFields.bindAutoCompletion(this.tfTitle,
-                new Callback<AutoCompletionBinding.ISuggestionRequest, Collection<String>>()
-                {
+                new Callback<AutoCompletionBinding.ISuggestionRequest, Collection<String>>() {
                     @Override
-                    public Collection<String> call(AutoCompletionBinding.ISuggestionRequest iSuggestionRequest)
-                    {
+                    public Collection<String> call(AutoCompletionBinding.ISuggestionRequest iSuggestionRequest) {
                         if (tfTitle.getText().length() <= 1) return Collections.emptyList();
                         return possibleSuggestions;
                     }
@@ -120,6 +117,51 @@ public class MainSceneController implements Initializable
 //            }
 //        );
 
+    }
+
+
+    /**This part is dedicated to drawer menu.*/
+    /**blurPane is a grey transparent pane used to create blur effect when menu is active
+     * this function set blurPane to not be visible by default.*/
+    @FXML
+    public void initialize() {
+        blurPane.setVisible(false);
+    }
+
+    @FXML
+    private AnchorPane drawerMenu;
+    @FXML
+    private Pane blurPane;
+
+    /**Activate drawer menu translateTransition for drawer menu, fadeTransition for blurPane.*/
+    @FXML
+    public void onMenuButton() {
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5),drawerMenu);
+        translateTransition.setByX(+235);
+        translateTransition.play();
+
+        blurPane.setVisible(true);
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5),blurPane);
+        fadeTransition.setFromValue(0);
+        fadeTransition.setToValue(1);
+        fadeTransition.play();
+    }
+
+    /**setOnFinished(lambdaExpression) to wait for the blur animation to finish before set blurPane invisible,
+     * otherwise, blurPane will disappear immediately.*/
+    @FXML
+    public void onMenuExit() {
+        TranslateTransition translateTransition = new TranslateTransition(Duration.seconds(0.5),drawerMenu);
+        translateTransition.setByX(-235);
+        translateTransition.play();
+
+        FadeTransition fadeTransition = new FadeTransition(Duration.seconds(0.5),blurPane);
+        fadeTransition.setFromValue(1);
+        fadeTransition.setToValue(0);
+        fadeTransition.play();
+
+        fadeTransition.setOnFinished(event -> {blurPane.setVisible(false);});
     }
 
 }
