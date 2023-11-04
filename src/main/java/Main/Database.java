ae033f323183e04a7da7219962410c2a2ad60a50
Tran Thai Duong <22028061@vnu.edu.vn>
2023-11-04 22:53:54 +0700

    Created database control class

--- src/main/java/Main/Database.java
+++ src/main/java/Main/Database.java
@@ -1,3 +1,24 @@
-package Main;public class Database {
+package Main;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    private static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection != null) {
+            connect();
+        }
+        return connection;
+    }
+
+    public static void connect() throws SQLException {
+        String url = ProjectDirectory.resourcesPath + "/data/anh_viet";
+
+        connection = DriverManager.getConnection(url);
+        System.out.println("Connected!");
+
+    }
 }
