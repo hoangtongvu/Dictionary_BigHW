fe1792e3307d89d6c6b804259ed37334389ed7d3
hoangtongvu <hoangtongvu@gmail.com>
2023-11-07 23:04:32 +0700

    Create CustomEvent.java

---
+++ src/main/java/Event/CustomEvent.java
@@ -0,0 +1,28 @@
+package Timer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomEvent
+{
+    private final List<Runnable> listeners;
+
+    public CustomEvent()
+    {
+        this.listeners = new ArrayList<>();
+    }
+
+    public void Invoke() { this.listeners.forEach(Runnable::run); }
+
+    public void AddListener(Runnable runnable)
+    {
+        this.listeners.add(runnable);
+    }
+
+    public void RemoveListener(Runnable runnable)
+    {
+        this.listeners.remove(runnable);
+    }
+
+
+}
