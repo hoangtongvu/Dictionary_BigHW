8464309daf115b0da4fd5c7040cd2d2e3ac46fe0
hoangtongvu <hoangtongvu@gmail.com>
2023-11-10 09:13:38 +0700

    Create StartGameScreen.

---
+++ src/main/java/Main/SceneControllers/StartGameScreenSceneController.java
@@ -0,0 +1,25 @@
+package Main.SceneControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class StartGameScreenSceneController
+{
+
+    @FXML
+    private TextField maxTimeTextField;
+
+    @FXML
+    private TextField numOfQuesTextField;
+
+    @FXML
+    private Button startGameButton;
+
+    private void MoveToGameScene()
+    {
+        
+    }
+
+}
