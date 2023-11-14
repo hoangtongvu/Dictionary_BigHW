8b19974941e9cf4a87f5631f9b601c5a53095e9d
Tran Thai Duong <22028061@vnu.edu.vn>
2023-11-14 15:45:40 +0700

    Renamed controller

--- src/main/java/Main/SceneControllers/Dictionary/DictionarySceneController.java
+++ src/main/java/Main/SceneControllers/Dictionary/DictionarySceneController.java
@@ -22,7 +22,7 @@ import java.sql.SQLException;
 import java.util.*;
 
 
-public class MainSceneController implements Initializable {
+public class DictionarySceneController implements Initializable {
 
     //private Timer timer;
     //private TimerTask timerTask;
