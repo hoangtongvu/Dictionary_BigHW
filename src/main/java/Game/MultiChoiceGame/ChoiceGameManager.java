eebfcb538dbfd4ddce651bc7c6554357b65c9c95
hoangtongvu <hoangtongvu@gmail.com>
2023-11-28 11:13:44 +0700

    few changes.

--- src/main/java/Game/MultiChoiceGame/ChoiceGameManager.java
+++ src/main/java/Game/MultiChoiceGame/ChoiceGameManager.java
@@ -11,9 +11,6 @@ public class ChoiceGameManager
 
     private final ChoiceGameCtrl choiceGameCtrl;
     private final List<MultiChoiceQues> questions;
-    private int maxQues = 20;
-    private int currentQuesPos = 0;
-    private ChoiceCode[] userAnswers;
 
 
     public final CustomEvent<MultiChoiceQues> onQuesChangeEvent;
@@ -32,43 +29,13 @@ public class ChoiceGameManager
 
     public void Start(int maxQues)
     {
-        this.LoadQuestions();
-        this.maxQues = maxQues;
-        this.userAnswers = new ChoiceCode[this.maxQues];
+        this.LoadQuestions(maxQues);
     }
 
-    public void End()
+    private void LoadQuestions(int maxQues)
     {
-
+        this.choiceGameCtrl.getChoiceQuesGenerator().GenerateRandomQuestions(maxQues);
     }
 
-    private void LoadQuestions()
-    {
-        this.choiceGameCtrl.getChoiceQuesGenerator().GenerateRandomQuestions(this.maxQues);
-    }
-
-    private MultiChoiceQues GetCurrentQuestion()
-    {
-        return this.questions.get(this.currentQuesPos);
-    }
-
-//    private void MoveToQuestionAt(int i)
-//    {
-//        if (i >= this.maxQues) return;
-//        this.currentQuesPos = i;
-//        this.onQuesChangeEvent.Invoke(this, this.GetCurrentQuestion());
-//        this.ClearChoseAnswer();
-//        this.TickAnswerBasedOnUserAnswer();
-//
-//        this.SetQuestionText(this.questions.get(this.currentQuesPos).getQuestion());
-//        this.SetAnswersCheckBox(this.questions.get(this.currentQuesPos).getAnswers());
-//        this.SetAnswerResultVbox();
-//    }
-
-    private boolean AnswerIsCorrect(int i)
-    {
-        ChoiceCode userAnswer = this.userAnswers[i];
-        return this.questions.get(i).CheckAnswer(userAnswer);
-    }
 
 }
